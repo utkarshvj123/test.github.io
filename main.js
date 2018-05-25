448366ab744c67ccf3a1c035c74fbe44cc66973f
utkarshvj123 <utkarshvj123@gmail.com>
2018-05-26 05:03:04 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(()=>{
+	    	var height = $(window).height();
+	    	$('.left-chat').css('height', (height - 92) + 'px');
+	    	$('.right-header-contentChat').css('height', (height - 163) + 'px');
+	    });
+		
+		myFunction1=()=> {
+    location.href = "profile.html";
+}
---
+++ profile.js
@@ -0,0 +1,3 @@
+myFunction=()=> {
+    location.href = "index.html";
+}
